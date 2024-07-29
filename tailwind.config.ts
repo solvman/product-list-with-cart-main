caeaffa84ee1256d5ffaf53166101b8ad62b83cd
solvman <solvman@gmail.com>
2024-07-29 16:59:04 -0500

    chore: Update font, text presets, and spacing in tailwind configuration. Design system setup.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,34 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      black: "hsl(0, 0%, 0%)",
+      red: "hsl(14, 86%, 42%)",
+      green: "hsl(159, 69%, 38%)",
+      rose: {
+        50: "hsl(20, 50%, 98%)",
+        100: "hsl(13, 31%, 94%)",
+        300: "hsl(14, 25%, 72%)",
+        400: "hsl(7, 20%, 60%)",
+        500: "hsl(12, 20%, 44%)",
+        900: "hsl(14, 65%, 9%)",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-red-hat-text)"]
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      spacing: {
+        50: "4px",
+        100: "8px",
+        150: "12px",
+        200: "16px",
+        300: "24px",
+        400: "32px",
+        500: "40px",
+        1100: "88px",
       },
     },
   },
