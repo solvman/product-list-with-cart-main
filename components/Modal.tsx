276ea1d5503d6e3b06e278eb1d3de3949ca48680
solvman <solvman@gmail.com>
2024-08-01 08:30:26 -0500

    refactor: Update Modal component with improved animation and scroll prevention

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -10,24 +10,31 @@ function Modal() {
   return (
     <AnimatePresence>
       {isOpen && (
-        <div className="absolute inset-0 bg-black/50">
-          <div className="fixed h-screen w-full overflow-y-auto">
+        // Modal Overlay
+        <div className="fixed inset-0">
+          <motion.div
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.125, ease: "easeInOut" }}
+            className="absolute inset-0 overflow-y-auto bg-black/50"
+          >
             {/* Modal */}
             <motion.div
               animate={{ opacity: 1, top: "10%" }}
               initial={{ opacity: 0, top: "100%" }}
               exit={{ opacity: 0, top: "100%" }}
               transition={{
-                delay: 0.1,
+                duration: 0.125,
                 type: "spring",
-                damping: 25,
-                stiffness: 300,
+                damping: 18,
+                stiffness: 200,
               }}
               className="relative -right-1/2 top-full flex w-full -translate-x-1/2 flex-col rounded-t-xl bg-white px-300 py-500 shadow-lg md:rounded-xl md:py-500 lg:max-w-[592px]"
             >
               <div>{modalContent}</div>
             </motion.div>
-          </div>
+          </motion.div>
         </div>
       )}
     </AnimatePresence>
